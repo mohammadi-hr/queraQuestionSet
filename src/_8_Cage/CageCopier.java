d58dd88f1b7d6a13625f6538936a1244103e331e
mohammadi-hr <ir.mohammadi.hr@gmail.com>
2019-03-10 19:21:04 +0330

    Cargo class until score = 16 of 25

--- src/_8_Cage/CageCopier.java
+++ src/_8_Cage/CageCopier.java
@@ -3,12 +3,24 @@ package _8_Cage;
 public class CageCopier {
 
     public void copy(Cage c1, Cage c2) {
-        if (c1 != null && c2 != null){
+        if (c1 != null && c2 != null) {
+            c2.setId(c1.getId());
+            c2.setName(new String(c1.getName()));
+            if (c1.getBirds() == null)
+                c2.setBirds(null);
+            else{
+                if(c1.getBirds()[0] != null && c2.getBirds()[0] == null){
+                    c2.getBirds()[0] = c1.getBirds()[0];
+                }else{
+                    c2.getBirds()[0] = null;
+                }
 
-            c2.setName(c1.getName());
-            Bird[] birds = new Bird[2];
-            birds = c1.getBirds();
-            c2.setBirds(birds);
+                if(c1.getBirds()[1] != null && c2.getBirds()[1] == null){
+                    c2.getBirds()[1] = c1.getBirds()[1];
+                }else {
+                    c2.getBirds()[1] = null;
+                }
+            }
 
         }
     }
