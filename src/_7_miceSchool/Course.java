a49e79518fc54f04d57b792a93a374787640eb76
mohammadi-hr <ir.mohammadi.hr@gmail.com>
2019-03-10 12:33:05 +0330

    miceSchool Class implemented, score = 12 of 20

--- src/_7_miceSchool/Course.java
+++ src/_7_miceSchool/Course.java
@@ -9,27 +9,35 @@ public class Course {
 
     private String name = new String();
     private Student[] students = new Student[10];
-    static int seatNumber = 0;
+    private static int seatNumber = 0;
 
     public boolean register(Student s) {
-        if (seatNumber != 9) {
+        if (seatNumber != 10) {
             this.students[seatNumber] = s;
+            seatNumber ++;
             return true;
         } else
             return false;
     }
 
     public int getNumOfStudents() {
-        return seatNumber + 1;
+        return seatNumber ;
     }
 
     public Student[] getStudents() {
         Student[] registeredStudents = new Student[seatNumber];
-        for (int i = 0; i <= seatNumber; i++) {
+        for (int i = 0; i < seatNumber; i++) {
             registeredStudents[i] = students[i];
         }
         return registeredStudents;
     }
+
+   /* public void printRegisteredStudents(){
+        Student[] students = getStudents();
+        for (int i = 0; i < students.length; i++){
+            System.out.println(students[i].getFirstName());
+        }
+    }*/
 
     public String getName() {
         return this.name;
